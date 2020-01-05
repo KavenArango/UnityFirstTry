1c493a4a5766c2254a0f9d973511a794a73b7657
Kaven arango <43826797+KavenArango@users.noreply.github.com>
2020-01-04 23:54:43 -0500

    added movement

---
+++ Assets/Scrips/Player Scrips/PlayerCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    public Transform transform;
+
+    void OnCollisionEnter(UnityEngine.Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            transform.position = movement.startPosition;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scrips/Player Scrips/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform transform;
+    public float forwadForce = 2000f;
+    public float sidewaysForce = 500f;
+    public Vector3 startPosition;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = (transform.position);
+    }
+
+    // Update is called once per frame for physics
+    void FixedUpdate()
+    {
+        float baseNum = 1 * Time.deltaTime;
+
+        rb.AddForce(0, 0 * baseNum, forwadForce * baseNum, ForceMode.VelocityChange);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * baseNum, 0, 0, ForceMode.VelocityChange);
+        }
+        else
+        {    
+            if (Input.GetKey("a"))
+            {
+                rb.AddForce(-sidewaysForce * baseNum, 0, 0, ForceMode.VelocityChange);
+            }
+        }
+    }
+}
