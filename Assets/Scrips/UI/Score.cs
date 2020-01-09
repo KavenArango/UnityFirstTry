4fef59f385ef523110200bb0d020b73b3c4a345e
KavenArango <kaven.arango@snhu.edu>
2020-01-09 18:25:16 -0500

    made new blocks and fog

---
+++ Assets/Scrips/UI/Score.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player.position.z;
+    }
+}
